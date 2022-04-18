9a7fbe7076544091c2ab0825dd344cf4fd11adc5
CreativeCrown <becreate.0906@gmail.com>
2022-04-18 20:48:26 +0900

    コメント追加

--- course.py
+++ course.py
@@ -9,7 +9,6 @@ class Way:
         print(f'{self.curve},{self.updown}')
 
 
-
 #Courseクラスの作成
 class Course:
     #Wayクラスのリストを格納するための変数を作成
@@ -23,6 +22,7 @@ class Course:
     def __iter__(self):
         return iter(self.data)
 
+    #追加メソッド
     def add(self, way):
         self.data.append(way)
 
@@ -34,24 +34,23 @@ class Course:
     def __setitem__(self, key, value):
         self.data[key] = value
 
+    #コースデータを作るメソッド
     def make_course(self, BOARD, object_right, object_left):
-        for i in range(self.CLEN):
-            lr1 = self.DATA_LR[i]
-            lr2 = self.DATA_LR[(i+1)%self.CLEN]
-            ud1 = self.DATA_UD[i]
-            ud2 = self.DATA_UD[(i+1)%self.CLEN]
-            for j in range(BOARD):
-                pos = j+BOARD*i 
-                self.add(Way(lr1*(BOARD-j)/BOARD + lr2*j/BOARD, ud1*(BOARD-j)/BOARD + ud2*j/BOARD))
-                # curve[pos] = lr1*(BOARD-j)/BOARD + lr2*j/BOARD
-                # updown[pos] = ud1*(BOARD-j)/BOARD + ud2*j/BOARD
-                if j == 60:
-                    object_right[pos] = 1 # 看板
-                if i%8 < 7:
-                    if j%12 == 0:
-                        object_left[pos] = 2 # ヤシの木
-                else:
-                    if j%20 == 0:
-                        object_left[pos] = 3 # ヨット
-                if j%12 == 6:
-                    object_left[pos] = 9 # 海
+        for i in range(self.CLEN):  #コースの長さ分(39回)繰り返す
+            lr1 = self.DATA_LR[i]       #カーブデータをlr1に代入
+            lr2 = self.DATA_LR[(i+1)%self.CLEN]    #次のカーブデータをlr2に代入
+            ud1 = self.DATA_UD[i]    #起伏データをudlに代入
+            ud2 = self.DATA_UD[(i+1)%self.CLEN]   #次の起伏データをud2に代入
+            for j in range(BOARD):  #板の数だけ(120回)繰り返す
+                pos = j+BOARD*i     #リストの添え字を計算しposに代入
+                self.add(Way(lr1*(BOARD-j)/BOARD + lr2*j/BOARD, ud1*(BOARD-j)/BOARD + ud2*j/BOARD)) #coursedataに道の曲がる向きと起伏を計算しリストに追加
+                if j == 60: #繰り返しの変数jが60なら
+                    object_right[pos] = 1   #道路右側に看板を置く
+                if i%8 < 7: #繰り返しの変数i%8<7の時
+                    if j%12 == 0:   #j%12が0の時に
+                        object_left[pos] = 2    #ヤシの木を置く
+                else:   #そうでなければ
+                    if j%20 == 0:   #j%20が0の時に
+                        object_left[pos] = 3    #ヨットを置く
+                if j%12 == 6:   #j%12が6の時に
+                    object_left[pos] = 9    #海を置く
