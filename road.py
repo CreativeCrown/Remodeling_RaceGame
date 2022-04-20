26374d573a85d41bc8a1c5cdf2a6c63f5aa5548f
CreativeCrown <becreate.0906@gmail.com>
2022-04-20 23:04:54 +0900

    make_curveとmake_updownメソッドを追加

--- road.py
+++ road.py
@@ -15,3 +15,15 @@ class Board:
             self.BOARD_W[i] = 10+(self.BOARD-i)*(self.BOARD-i)/12  #幅を計算
             self.BOARD_H[i] = 3.4*(self.BOARD-i)/self.BOARD    #高さを計算
             self.BOARD_UD[i] = 2*math.sin(math.radians(i*1.5))   #起伏の値を三角関数で計算
+
+    #描画用の道路のX座標(道の曲がり具合)を計算するメソッド
+    def make_curve(self, di, board_x, coursedata, cars, CMAX):
+        for i in range(self.BOARD):
+            di[0] += coursedata[int(cars[0].y+i)%CMAX].curve    #カーブデータから道の曲がりを計算
+            board_x[i] = 400 - self.BOARD_W[i]*cars[0].x/800 + di[0]/2  #板のX座標を計算し代入
+            
+    #描画用の道路の路面の起伏(高低差)を計算するメソッド
+    def make_updown(self, ud, board_ud, coursedata, cars, CMAX):
+        for i in range(self.BOARD):
+            ud[0] += coursedata[int(cars[0].y+i)%CMAX].updown   #起伏データから起伏を計算
+            board_ud[i] = ud[0]/30 #板の高低を計算し代入
