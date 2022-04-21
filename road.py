43d9cefeb3bdbdd5e951c79b96a0e1df5b9bec8a
CreativeCrown <becreate.0906@gmail.com>
2022-04-21 22:50:05 +0900

    Boardクラスのコンストラクタにboard_x,board_udの追加

--- road.py
+++ road.py
@@ -3,11 +3,13 @@ import math
 #道路を作る板の情報を持つBoardクラスを作成
 class Board:
     #コンストラクタ
-    def __init__(self, BOARD, BOARD_W, BOARD_H, BOARD_UD):
+    def __init__(self, BOARD, BOARD_W, BOARD_H, BOARD_UD, board_x, board_ud):
         self.BOARD = BOARD
         self.BOARD_W = BOARD_W
         self.BOARD_H = BOARD_H
         self.BOARD_UD = BOARD_UD
+        self.board_x = board_x
+        self.board_ud = board_ud
 
     #道路の板の基本形状を計算するメソッド
     def roadbasic(self):
@@ -17,13 +19,13 @@ class Board:
             self.BOARD_UD[i] = 2*math.sin(math.radians(i*1.5))   #起伏の値を三角関数で計算
 
     #描画用の道路のX座標(道の曲がり具合)を計算するメソッド
-    def make_curve(self, di, board_x, coursedata, cars, CMAX):
+    def make_curve(self, di, coursedata, cars, CMAX):
         for i in range(self.BOARD):
             di[0] += coursedata[int(cars[0].y+i)%CMAX].curve    #カーブデータから道の曲がりを計算
-            board_x[i] = 400 - self.BOARD_W[i]*cars[0].x/800 + di[0]/2  #板のX座標を計算し代入
+            self.board_x[i] = 400 - self.BOARD_W[i]*cars[0].x/800 + di[0]/2  #板のX座標を計算し代入
             
     #描画用の道路の路面の起伏(高低差)を計算するメソッド
-    def make_updown(self, ud, board_ud, coursedata, cars, CMAX):
+    def make_updown(self, ud, coursedata, cars, CMAX):
         for i in range(self.BOARD):
             ud[0] += coursedata[int(cars[0].y+i)%CMAX].updown   #起伏データから起伏を計算
-            board_ud[i] = ud[0]/30 #板の高低を計算し代入
+            self.board_ud[i] = ud[0]/30 #板の高低を計算し代入
