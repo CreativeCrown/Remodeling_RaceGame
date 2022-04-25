80250acc4dcedc977b98d95bde0e5b81d6001383
CreativeCrown <becreate.0906@gmail.com>
2022-04-26 01:16:55 +0900

    板の幅を少し大きく調整

--- road.py
+++ road.py
@@ -16,7 +16,7 @@ class Board:
     #道路の板の基本形状を計算するメソッド
     def roadbasic(self):
         for i in range(self.BOARD):
-            self.BOARD_W[i] = 10+(self.BOARD-i)*(self.BOARD-i)/12  #幅を計算
+            self.BOARD_W[i] = 10+(self.BOARD-i)*(self.BOARD-i)/10  #幅を計算
             self.BOARD_H[i] = 3.4*(self.BOARD-i)/self.BOARD    #高さを計算
             self.BOARD_UD[i] = 2*math.sin(math.radians(i*1.5))   #起伏の値を三角関数で計算
 
