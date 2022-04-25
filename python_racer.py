08af0ab68173b0b203a60a1c0cd4673012116547
CreativeCrown <becreate.0906@gmail.com>
2022-04-26 01:12:33 +0900

    コースの1lapごとの長さを定めるCLAPMAX定数を作成

--- python_racer.py
+++ python_racer.py
@@ -14,11 +14,28 @@ rec = [0]     #走行時間を測る変数
 recbk = [0]   #ラップタイム計算用の変数
 se_crash = None #衝突時の効果音を読み込む変数
 
-DATA_LR = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 2, 1, 0, 2, 4, 2, 4, 2, 0, 0, 0, -2, -2, -4, -4, -2, -1, 0, 0, 0, 0, 0, 0, 0]     #道路のカーブを作る下になるデータ
-DATA_UD = [0, 0, 1, 2, 3, 2, 1, 0, -2, -4, -2, 0, 0, 0, 0, 0, -1, -2, -3, -4, -3, -2, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -3, 3, 0, -6, 6, 0]  #道路の起伏を作る基になるデータ
-CLEN = len(DATA_LR)     #これらのデータの要素数を代入した定数
+#道路のカーブを作る下になるデータ
+DATA_LR = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 2, 1, 0, 2, 4, 2, 4, 2, 0, 0, 0, -2, -2, -4, -4, -2, -1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -2, -3, -2, -1, 0, -2, -4, -2, -4, -2, 0, 0, 0, 2, 2, 4, 4, 2, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 6, 4, 2, 0, 4, 8, 4, 8, 4, 0, 0, 0, -4, -4, -8, -8, -4, -2, 0, 0, 0, 0, 0, 0, 0],
+    ]
+#道路の起伏を作る基になるデータ
+DATA_UD = [
+    [0, 0, 2, 2, 4, 4, 2, 0, -4, -4, -4, 0, 0, 0, 0, 0, -2, -4, -4, -4, -4, -4, -2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -4, 4, 0, -4, 4, 0],
+    [0, 0, 1, 2, 3, 2, 1, 0, -2, -4, -2, 0, 0, 0, 0, 0, -1, -2, -3, -4, -3, -2, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -3, 3, 0, -4, 4, 0],
+    [0, 0, -1, -2, -3, -2, -1, 0, 2, 4, 2, 0, 0, 0, 0, 0, 1, 2, 3, 4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, -3, 0, 4, -4, 0],
+    ]
+CLAPSLEN = len(DATA_LR[0])     #これらのデータの要素数を代入した定数
+CLEN = len(DATA_LR[0])*3
+
+# for i in range(len(DATA_LR)):
+#     print(len(DATA_LR[i]))
+# for j in range(len(DATA_UD)):
+#     print(len(DATA_UD[j]))
 
 BOARD = 120         #道路を描く板の枚数を定める定数
+CLAPMAX = BOARD*CLAPSLEN   #1lapの長さを定める定数
 CMAX = BOARD*CLEN   #コースの長さ(要素数)を定める定数
 object_left = [0]*CMAX  #道路左にある物体の番号を入れるリスト
 object_right = [0]*CMAX #道路右にある物体の番号を入れるリスト
@@ -38,9 +55,18 @@ for i in range(1, CAR):
 LAPS = 3                    #何周すればゴールかを定める定数
 laptime = ["0'00.00"]*LAPS  #ラップタイム表示用のリスト
 
-#Courseクラスのインスタンスを作成
-cdata = Course(CMAX, CLEN, 3, 0, ["0'00.00"]*LAPS, DATA_LR, 
-                        DATA_UD, object_left, object_right)
+
+
+#Courseクラスをcourse変数に格納
+cdata = Course(CMAX, CLEN, CLAPSLEN, CLAPMAX, DATA_LR, 
+                            DATA_UD, object_left, object_right)
+
+#Course_typeクラスのインスタンスを作成
+ctype = Course_type(LAPS, laptime)
+
+#Courseクラスのインスタンスを格納
+ctype.add(cdata)
+
 #Boardクラスのインスタンスを作成
 board = Board(BOARD, BOARD_W, BOARD_H, BOARD_UD, board_x, board_ud)
 #Transitionクラスのインスタンスを作成
@@ -48,7 +74,7 @@ transi = Transition(0, 0)
 
 
 def main(): #メイン処理を行う関数
-    global idx, tmr, laps, rec, recbk, se_crash, mycar  #これらをグローバル変数とする
+    global rec, recbk, se_crash  #これらをグローバル変数とする
     pygame.init()   #pygameモジュールの初期化
     pygame.display.set_caption("Python Racer")  #ウィンドウに表示するタイトルを指定
     screen = pygame.display.set_mode((800, 600))    #描画面を初期化
@@ -57,6 +83,7 @@ def main(): #メイン処理を行う関数
     fnt_m = pygame.font.Font(None, 50)  #フォントオブジェクトを作成、中位の文字
     fnt_l = pygame.font.Font(None, 120) #フォントオブジェクトを作成、大きな文字
     d_item = Draw(screen, fnt_s, fnt_m, fnt_l) #drawオブジェクトを作成
+    c = 0   #実際に走るコースの種類用のインデックス
 
     img_title = pygame.image.load("image_pr/title.png").convert_alpha() #タイトルロゴを読み込む変数
     img_bg = pygame.image.load("image_pr/bg.png").convert() #背景(空と地面の絵)を読み込む変数
@@ -113,12 +140,14 @@ def main(): #メイン処理を行う関数
                     screen = pygame.display.set_mode((800, 600))    #通常表示に戻す
                     d_item.screen = screen    #インスタンスを初期化
         transi.tmr += 1    #tmrの値を1増やす
+    
 
         #描画用の道路のX座標と路面の高低を計算
         board.di = 0  #道が曲がる向きを計算する変数、加えて毎回初期化
         board.ud = 0  #道の起伏を計算する変数、加えて毎回初期化
         board.make_curve(cdata, cars)   #道の曲がり具合を計算
         board.make_updown(cdata, cars) #道の起伏を計算
+
 
         d_item.make_horizon(board) #地平線のY座標を計算しhorizonに代入
         d_item.sy = d_item.horizon    #背景の垂直を計算し、道路を描き始めるY座標をsyに代入
@@ -131,16 +160,16 @@ def main(): #メイン処理を行う関数
         d_item.screen.blit(img_sea, [board_x[BOARD-1]-780, d_item.sy])    #左手奥の海を描画
 
         #描画用データをもとに道路を描く
-        d_item.draw_road(board, cars, cdata, img_obj, img_sea, img_car)
+        d_item.draw_road(board, cars, cdata, ctype, img_obj, img_sea, img_car)
 
         d_item.draw_text(str(int(cars[0].spd)) + "km/h", 680, 30, d_item.RED, fnt_m)   #速度を表示
-        d_item.draw_text("lap {}/{}".format(cdata.laps, cdata.LAPS), 100, 30, d_item.WHITE, fnt_m)    #周回数を表示
+        d_item.draw_text("lap {}/{}".format(cdata.laps, ctype.LAPS), 100, 30, d_item.WHITE, fnt_m)    #周回数を表示
         d_item.draw_text("time "+time_str(rec[0]), 100, 80, d_item.GREEN, fnt_s) #タイムを表示
-        for i in range(cdata.LAPS):   #繰り返しで
-            d_item.draw_text(cdata.laptime[i], 80, 130+40*i, d_item.YELLOW, fnt_s)  #ラップタイムを表示
+        for i in range(ctype.LAPS):   #繰り返しで
+            d_item.draw_text(ctype.laptime[i], 80, 130+40*i, d_item.YELLOW, fnt_s)  #ラップタイムを表示
 
         key = pygame.key.get_pressed()  #keyに全てのキーの状態を代入
-        transi.game_transition(cars, cdata, d_item, img_title, img_car, se_crash, rec, recbk, key)
+        transi.game_transition(cars, ctype, cdata, d_item, board, img_title, img_car, se_crash, rec, recbk, key)
 
         pygame.display.update() #画面を更新する
         clock.tick(60)  #フレームレートを指定
