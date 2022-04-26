20c71e7165c51b413fe9b6a3c2def4dd7d4ac8e0
CreativeCrown <becreate.0906@gmail.com>
2022-04-27 04:00:05 +0900

    チュートリアル用のファイルとクラスを作成

---
+++ tutorial.py
@@ -0,0 +1,43 @@
+#チュートリアル用クラス
+class Tutorial:
+    #コンストラクタ
+    def __init__(self):
+        self.__btna = 0
+        self.__btnz = 0
+        self.__btnl = 0
+        self.__btnr = 0
+
+    #ゲッター
+    @property
+    def btna(self):
+        return self.__btna
+
+    @property
+    def btnz(self):
+        return self.__btnz
+
+    @property
+    def btnl(self):
+        return self.__btnl
+
+    @property
+    def btnr(self):
+        return self.__btnr
+
+
+    #セッター
+    @btna.setter
+    def btna(self, value):
+        self.__btna = value
+
+    @btnz.setter
+    def btnz(self, value):
+        self.__btnz = value
+
+    @btnl.setter
+    def btnl(self, value):
+        self.__btnl = value
+
+    @btnr.setter
+    def btnr(self, value):
+        self.__btnr = value
